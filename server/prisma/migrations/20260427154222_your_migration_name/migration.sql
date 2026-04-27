eeb45eb0578281cd2797fb9524c801e1c370e7f9
omarsanad100 <omarsanad.me@gmail.com>
2026-04-27 19:34:49 +0300

    Add migration for Post model: create Post table with foreign key constraints for userId and parentId to enhance data relationships.

---
+++ server/prisma/migrations/20260427154222_your_migration_name/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL,
+    "value" DOUBLE PRECISION NOT NULL,
+    "parentId" TEXT,
+    "operation" TEXT,
+    "right" DOUBLE PRECISION,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_parentId_fkey" FOREIGN KEY ("parentId") REFERENCES "Post"("id") ON DELETE SET NULL ON UPDATE CASCADE;
