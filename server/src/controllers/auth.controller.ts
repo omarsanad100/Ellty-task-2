3292135d94fb39b749d10994e78b97da8d73eb71
omarsanad100 <omarsanad.me@gmail.com>
2026-04-27 19:37:44 +0300

    Remove unused authentication and calculation controllers and routes to streamline the codebase and improve maintainability.

--- server/src/controllers/auth.controller.ts
+++
@@ -1,5 +0,0 @@
-import type { Request, Response } from 'express'
-
-export const testController = async (req: Request, res: Response) => {
-  return res.json({ message: 'User successfully logged in' })
-}
--- server/src/routes/auth.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import { testController } from './../controllers/auth.controller.js'
-
-const router = Router()
-
-router.get('/login', testController)
-
-export default router
