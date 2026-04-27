514cc287844f280a932c3bbf6a947f06dc96ecf3
omarsanad100 <omarsanad.me@gmail.com>
2026-04-27 19:35:05 +0300

    Refactor server structure: replace test routes with authentication and post routes, integrate error middleware, and update server port configuration for improved organization and functionality.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,18 +1,19 @@
+import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
-import dotenv from 'dotenv'
-import testRoutes from './routes/auth.routes.js'
-import { connectDB, disconnectDB } from './config/db.js'
 
-dotenv.config()
+import authRoutes from './auth/auth.routes.js'
+import postRoutes from './post/post.routes.js'
+import { errorMiddleware } from './middlewares/error.middleware.js'
 
 const app = express()
 
-// Middlewares
 app.use(cors())
 app.use(express.json())
 
-// Test route
-app.use('/api', testRoutes)
+app.use('/auth', authRoutes)
+app.use('/posts', postRoutes)
+
+app.use(errorMiddleware)
 
 export default app
